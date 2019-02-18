596a1a0a60f858f4461eb51740db4b5fc1b3e069
Habeeb Okunade <guessy2k2@yahoo.com>
2019-02-19 10:36:15 +1100

    Completed Authorization Server

---
+++ src/main/java/com/habeeb/cycle/webapisecurityoauth2/Webapisecurityoauth2Application.java
@@ -0,0 +1,13 @@
+package com.habeeb.cycle.webapisecurityoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webapisecurityoauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Webapisecurityoauth2Application.class, args);
+    }
+
+}
