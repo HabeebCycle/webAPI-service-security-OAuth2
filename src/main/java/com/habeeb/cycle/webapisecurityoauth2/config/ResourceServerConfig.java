43635e2b18100f9fe0e78227ca07f66f952f1097
Habeeb Okunade <guessy2k2@yahoo.com>
2019-02-19 11:49:55 +1100

    Completed Authorization Server

--- src/main/java/com/habeeb/cycle/webapisecurityoauth2/config/ResourceServerConfig.java
+++ src/main/java/com/habeeb/cycle/webapisecurityoauth2/config/ResourceServerConfig.java
@@ -1,6 +1,10 @@
 package com.habeeb.cycle.webapisecurityoauth2.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
@@ -8,6 +12,31 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableResourceServer
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
+    private AuthenticationManager authenticationManager;
 
+    public ResourceServerConfig(AuthenticationManager authenticationManager){
+        this.authenticationManager = authenticationManager;
+    }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.requestMatchers()
+                .antMatchers("/login", "/oauth/authorize")
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.parentAuthenticationManager(authenticationManager)
+                .inMemoryAuthentication()
+                .withUser("habeebcycle")
+                .password("habeeb")
+                .roles("ADMIN");
+    }
 }
