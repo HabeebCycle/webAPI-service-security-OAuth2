50f2ce27681da064872e2c12aac6b644e9a94250
Habeeb Okunade <guessy2k2@yahoo.com>
2019-02-19 11:06:24 +1100

    Completed Authorization Server

---
+++ src/main/java/com/habeeb/cycle/webapisecurityoauth2/config/ResourceServerConfig.java
@@ -0,0 +1,13 @@
+package com.habeeb.cycle.webapisecurityoauth2.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+
+    
+
+}
